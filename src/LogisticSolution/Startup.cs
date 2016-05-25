a58434b95427e98b127cb703d583ba3f62c0df2f
Vinay Joshi <vinay.joshi.ngi@gmail.com>
2016-05-25 23:56:25 +0530

    changed dashboard layout

--- src/LogisticSolution/Startup.cs
+++ src/LogisticSolution/Startup.cs
@@ -6,11 +6,26 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace LogisticSolution
 {
     public class Startup
     {
+
+        IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{ env.EnvironmentName }.json", optional: true);
+
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
